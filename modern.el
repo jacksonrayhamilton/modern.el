8708ed2ba4cf683c2f47fe289e4b107bfb8449c8
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2015-04-04 23:32:07 -0700

    Refactor using `modern-set-key'.

--- modern.el
+++ modern.el
@@ -23,6 +23,14 @@
   (dolist (key keys)
     (global-unset-key (kbd key))))
 
+(defun modern-set-key (shortcut command &optional keymap)
+  "Setup SHORTCUT for COMMAND, on KEYMAP or globally."
+  (cond
+   (keymap
+    (define-key keymap (kbd shortcut) command))
+   (t
+    (global-set-key (kbd shortcut) command))))
+
 ;;;###autoload
 (defun modern-enable ()
   "Replace Emacs's insane default shortcuts with modern ones.
@@ -40,55 +48,55 @@ restart Emacs."
   ;; looks like a down arrow; "h" is on the left, and "l" is on the right.  By
   ;; elimination, "k" must mean "up".
   (modern-unset-keys "C-M-f" "C-M-b" "C-p" "C-n") ; C-f, M-f, C-b, M-b are used
-  (global-set-key (kbd "C-h") 'backward-char)
-  (global-set-key (kbd "M-h") 'backward-word)
-  (global-set-key (kbd "C-M-h") 'backward-sexp)
-  (global-set-key (kbd "C-j") 'next-line)
-  (define-key comint-mode-map (kbd "M-j") 'comint-next-input)
-  (global-set-key (kbd "C-k") 'previous-line)
-  (define-key comint-mode-map (kbd "M-k") 'comint-previous-input)
-  (global-set-key (kbd "C-l") 'forward-char)
-  (global-set-key (kbd "M-l") 'forward-word)
-  (global-set-key (kbd "C-M-l") 'forward-sexp)
-  (define-key comint-mode-map (kbd "C-M-l") 'forward-sexp)
+  (modern-set-key "C-h" 'backward-char)
+  (modern-set-key "M-h" 'backward-word)
+  (modern-set-key "C-M-h" 'backward-sexp)
+  (modern-set-key "C-j" 'next-line)
+  (modern-set-key "M-j" 'comint-next-input comint-mode-map)
+  (modern-set-key "C-k" 'previous-line)
+  (modern-set-key "M-k" 'comint-previous-input comint-mode-map)
+  (modern-set-key "C-l" 'forward-char)
+  (modern-set-key "M-l" 'forward-word)
+  (modern-set-key "C-M-l" 'forward-sexp)
+  (modern-set-key "C-M-l" 'forward-sexp comint-mode-map)
 
   ;; Use C-, to move to the beginning of the line and C-. to move to the end.  ","
   ;; and "." share keys with "<" and ">", which look like left and right arrows,
   ;; which indicate the direction to move.
   (modern-unset-keys "M-a" "M-e") ; C-a and C-e are used
-  (global-set-key (kbd "C-,") 'move-beginning-of-line)
-  (global-set-key (kbd "C-.") 'move-end-of-line)
+  (modern-set-key "C-," 'move-beginning-of-line)
+  (modern-set-key "C-." 'move-end-of-line)
 
   ;; Use C-o to open [files], C-b to switch [buffers], C-s to save, and C-M-s to
   ;; save-as.
   (modern-unset-keys "C-x C-f" "C-x b" "C-x s" "C-x C-w" "C-x C-j")
-  (global-set-key (kbd "C-o") 'ido-find-file)
-  (define-key dired-mode-map (kbd "C-o") 'ido-find-file)
-  (global-set-key (kbd "C-b") 'ido-switch-buffer)
-  (global-set-key (kbd "C-s") 'save-buffer)
-  (global-set-key (kbd "C-M-s") 'ido-write-file)
-  (global-set-key (kbd "C-/") 'dired-jump)
+  (modern-set-key "C-o" 'ido-find-file)
+  (modern-set-key "C-o" 'ido-find-file dired-mode-map)
+  (modern-set-key "C-b" 'ido-switch-buffer)
+  (modern-set-key "C-s" 'save-buffer)
+  (modern-set-key "C-M-s" 'ido-write-file)
+  (modern-set-key "C-/" 'dired-jump)
   (add-hook
    'ido-setup-hook
    (lambda ()
-     (define-key ido-completion-map (kbd "C-x C-f") nil)
-     (define-key ido-completion-map (kbd "C-x C-w") nil)
-     (define-key ido-completion-map (kbd "C-o") 'ido-fallback-command)
-     (define-key ido-completion-map (kbd "C-M-s") 'ido-fallback-command)))
+     (modern-set-key "C-x C-f" nil ido-completion-map)
+     (modern-set-key "C-x C-w" nil ido-completion-map)
+     (modern-set-key "C-o" 'ido-fallback-command ido-completion-map)
+     (modern-set-key "C-M-s" 'ido-fallback-command ido-completion-map)))
 
   ;; Use C-w to close, like in a web browser.
   (modern-unset-keys "C-x k")
-  (global-set-key (kbd "C-w") 'kill-this-buffer)
+  (modern-set-key "C-w" 'kill-this-buffer)
 
   ;; Use C-f to find and C-r to find and replace.  Replace control with meta for
   ;; the regexp versions.
   (modern-unset-keys "M-%" "C-M-%") ; C-s, C-M-s are used
-  (global-set-key (kbd "C-f") 'isearch-forward)
-  (define-key isearch-mode-map (kbd "C-s") nil)
-  (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
-  (global-set-key (kbd "M-f") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-r") 'query-replace)
-  (global-set-key (kbd "M-r") 'query-replace-regexp)
+  (modern-set-key "C-f" 'isearch-forward)
+  (modern-set-key "C-s" nil isearch-mode-map)
+  (modern-set-key "C-f" 'isearch-repeat-forward isearch-mode-map)
+  (modern-set-key "M-f" 'isearch-forward-regexp)
+  (modern-set-key "C-r" 'query-replace)
+  (modern-set-key "M-r" 'query-replace-regexp)
 
   ;; Use C-0, C-1, C-2 and C-3 to manipulate windows.  (Exchange C-x with C.)
   ;; Use M-b to switch between windows; C-b is frequently used to "change
@@ -98,23 +106,23 @@ restart Emacs."
    "C-x 0" "C-x 1" "C-x 2" "C-x 3"
    "C-4" "C-5" "C-6" "C-7" "C-8" "C-9"
    "C-x o")
-  (global-set-key (kbd "C-0") 'delete-window)
-  (global-set-key (kbd "C-1") 'delete-other-windows)
-  (global-set-key (kbd "C-2") 'split-window-below)
-  (global-set-key (kbd "C-3") 'split-window-right)
-  (global-set-key (kbd "M-b") 'other-window)
+  (modern-set-key "C-0" 'delete-window)
+  (modern-set-key "C-1" 'delete-other-windows)
+  (modern-set-key "C-2" 'split-window-below)
+  (modern-set-key "C-3" 'split-window-right)
+  (modern-set-key "M-b" 'other-window)
 
   ;; Use C-a to select all.
   (modern-unset-keys "C-x h")
-  (global-set-key (kbd "C-a") 'mark-whole-buffer)
+  (modern-set-key "C-a" 'mark-whole-buffer)
 
   ;; Eval is fun; use C-e and M-e to evaluate expressions and buffers.
   (modern-unset-keys "C-x C-e")
-  (global-set-key (kbd "C-e") 'eval-last-sexp)
-  (global-set-key (kbd "M-e") 'eval-buffer)
+  (modern-set-key "C-e" 'eval-last-sexp)
+  (modern-set-key "M-e" 'eval-buffer)
 
   ;; Use C-M-d to kill expressions.
-  (global-set-key (kbd "C-M-d") 'kill-sexp))
+  (modern-set-key "C-M-d" 'kill-sexp))
 
 (provide 'modern)
 
